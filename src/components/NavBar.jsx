834bc502f804c2f9506ef0a36021b326ca549ead
Ciastelix <tenlegitmati@gmail.com>
2021-11-06 19:08:28 +0100

    fixed navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,23 @@
 import React, {useState } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import styles from "./NavBar.module.css"
-import { IoIosLogIn, IoIosLogOut, IoHomeOutline, IoSearchSharp } from "react-icons/io";
+
 import Cookies from 'universal-cookie'
 
 export default function () {
     let hist = useHistory()
     const cookies = new Cookies();
     const search = () => {
-        hist.push(`/search/user/${name}`)
+        hist.push(`/user/search/${name}`)
     } 
     const [name, setName] = useState("")
     return (
         <nav>
             <div className={styles.NavBarContainer}>
             <ul className={styles.NavBar}>
-                <li className={styles.NavItem}><Link className={styles.Link} to="/"><IoHomeOutline/> home page</Link></li>
-                <li className={styles.NavItem}><Link className={styles.Link} to="/add/post"><BsPlusLg /> add post</Link></li>
+                <li className={styles.NavItem}><Link className={styles.Link} to="/"><ion-icon name="home-outline"></ion-icon> home page</Link></li>
+                <li className={styles.NavItem}><Link className={styles.Link} to="/add/post"> add post</Link></li>
                 <li className={styles.NavItemS}><input onChange={e => setName(e.target.value)} className={styles.Search} type="text" placeholder="Search..."/><button onClick={search} className={styles.Btn}> Search</button></li>
-                <li className={styles.NavItemS}><input onChange={e => setName(e.target.value)} className={styles.Search} type="text" placeholder="Search..."/><button onClick={search} className={styles.Btn}><IoSearchSharp/> Search</button></li>
                 </ul>
             </div>
         </nav>
